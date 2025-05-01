d64a989584bffc625c36d3f202e2ce0a56cb7473
BigKnight53 <2907887843@qq.com>
2025-05-01 19:41:46 +0800

    Site updated: 2025-05-01 19:41:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/15/Binary Learning/","2024/11/17/C Algorithm/","2024/11/15/C Matrix/","2024/10/20/C Operators And Expressions/","2024/11/26/C Math Questions/","2024/10/19/C Statement/","2024/12/01/Excel Handbook/","2021/04/01/Hello world/","2022/08/09/Markdown Grammar/","2024/12/19/C Function/","2025/03/22/Linear Tables/","2024/09/23/My College life/","2023/05/02/Widgets/","2023/07/24/Windows 11 Update/"];function toRandomPost(){
+var posts=["2024/10/15/Binary Learning/","2024/11/17/C Algorithm/","2024/12/19/C Function/","2024/11/26/C Math Questions/","2024/11/15/C Matrix/","2024/10/20/C Operators And Expressions/","2024/10/19/C Statement/","2024/12/01/Excel Handbook/","2021/04/01/Hello world/","2025/03/22/Linear Tables/","2024/09/23/My College life/","2022/08/09/Markdown Grammar/","2023/05/02/Widgets/","2023/07/24/Windows 11 Update/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
